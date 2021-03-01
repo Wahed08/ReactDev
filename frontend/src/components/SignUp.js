132f4fe4c2a02133d259e5d386a8a45a6575be3c
Wahed08 <wahedfarhad123@gmail.com>
2021-03-02 01:32:53 +0600

    ReactApp

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -83,7 +83,6 @@ class SignUp extends Component {
             window.location = 'http://localhost:3000/login';
  }
               
-       
         render(){
         return (
              <div className="container">
@@ -93,22 +92,22 @@ class SignUp extends Component {
                 <p>Please fill in this form to create an account.</p>
 
                 <label for="name"><b>Name</b></label>
-                <input type="text" placeholder="Enter Name" value={this.state.name} onChange={this.handleNameChange} required></input>
+                <input type="text" placeholder="Enter Name" value={this.state.name} onChange={this.handleNameChange}></input>
 
                 <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" value={this.state.username} onChange={this.handleUsernameChange} required ></input>
+                <input type="text" placeholder="Enter Username" value={this.state.username} onChange={this.handleUsernameChange} ></input>
 
                 <label for="department"><b>Department</b></label>
-                <input type="text" placeholder="Enter Department" value={this.state.department} onChange={this.handleDepartmentChange} required ></input>
+                <input type="text" placeholder="Enter Department" value={this.state.department} onChange={this.handleDepartmentChange}></input>
 
                 <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" value={this.state.email} onChange={this.handleEmailChange} required ></input>
+                <input type="text" placeholder="Enter Email" value={this.state.email} onChange={this.handleEmailChange}></input>
 
                 <label for="password"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" value={this.state.password} onChange={this.handlePassChange} required ></input>
+                <input type="password" placeholder="Enter Password" value={this.state.password} onChange={this.handlePassChange} ></input>
 
                 <label for="condirm-password"><b>Confirm Pasword</b></label>
-                <input type="password" placeholder="Enter Confirm Password" value={this.state.confirm_password} onChange={this.handlePass2Change} required ></input>
+                <input type="password" placeholder="Enter Confirm Password" value={this.state.confirm_password} onChange={this.handlePass2Change} ></input>
 
                 <input type="submit" class="input" value="Submit" />
                 </form>
