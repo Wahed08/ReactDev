61dc3966c5cc73755fa5b6fccfc5bccc59d4312c
Wahed08 <wahedfarhad123@gmail.com>
2021-03-02 01:55:40 +0600

    Backend Registration

--- backend/DBconfig/DB_URL.js
+++ backend/DBconfig/DB_URL.js
@@ -1,4 +1,4 @@
 module.exports = {
     
-    URL: 'mongodb://Farhad:123farhad@node-shard-00-00.g4eui.mongodb.net:27017,node-shard-00-01.g4eui.mongodb.net:27017,node-shard-00-02.g4eui.mongodb.net:27017/Register?ssl=true&replicaSet=atlas-rywlaq-shard-0&authSource=admin&retryWrites=true&w=majority'
+    URL: 'DB_URL'
 }
--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -3,52 +3,25 @@ const bcrypt = require("bcryptjs");
 
 //signup handling
 const create_register = (req, res) => {
-  //const saltPassword = await bcrypt.genSalt(10);
-  // const securePassword = await bcrypt.hash(req.body.password, saltPassword);
+  const saltPassword = await bcrypt.genSalt(10);
+  const securePassword = await bcrypt.hash(req.body.password, saltPassword);
 
-  // const Model = new model({
-  //     name : req.body.name,
-  //     username: req.body.username,
-  //     department: req.body.department,
-  //     email: req.body.email,
-  //     password: securePassword,
-  //     confirm_password: securePassword
-  // });
+  const Model = new model({
+      name : req.body.name,
+      username: req.body.username,
+      department: req.body.department,
+      email: req.body.email,
+      password: securePassword,
+      confirm_password: securePassword
+  });
 
-  // Model.save()
-  // .then(data=>{
-  //     res.json(data);
-  // }).catch(err=>{
-  //     res.json(err);
-  // })
-  const { name, username, department, email, password, password2 } = req.body;
-  const errors = [];
-  //check required fields
-
-  if (!name || !username || !department || !email || !password || !password2) {
-    errors.push({ msg: "Please fill all the fields" });
-  }
-
-  //check password match
-  if (password !== password2) {
-    errors.push({ msg: "Passwords do not match" });
-  }
-  //check password length
-  if (password.length < 6) {
-    errors.push({ msg: "Password should be at least 6 characters" });
-  }
-if (errors.length > 0) {
-    console.log("vul hoiche");
-    res.render("http://localhost:3000/signup", {
-                errors,
-                name,
-                username,
-                department,
-                email,
-                password,
-                password2,
-    });
-  }
+  Model.save()
+  .then(data=>{
+      res.json(data);
+  }).catch(err=>{
+      res.json(err);
+  })
+ 
 };
 
 module.exports = { create_register };
